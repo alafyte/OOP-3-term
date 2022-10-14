afd512f2e80b27cd293ba442ec2fe244401cbdb4
alafyte <nroussak0405@gmail.com>
2022-10-14 21:25:14 +0300

    Первое задание

---
+++ Lab09/Lab09/GeometricFiguresCollection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Lab09
+{
+    /*1) Реализуйте указанный интерфейс и другие при необходимости, соберите объекты класса в 
+    коллекцию (можно сделать специальных класс с вложенной коллекцией и методами ею управляющими)*/
+    class GeometricFiguresCollection : IEnumerator, IEnumerable
+    {
+        private GeometricFigure[] _geometricFigures;
+        private int _index;
+
+        public GeometricFiguresCollection()
+        {
+            _index = -1;
+            _geometricFigures = Array.Empty<GeometricFigure>();
+        }
+
+
+        //Реализация интерфейсов
+        public bool MoveNext()
+        {
+            if (_index == _geometricFigures.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+
+            _index++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return _geometricFigures[_index];
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (var i = 0; i < _geometricFigures.Length; i++)
+                yield return _geometricFigures[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //Другие методы
+        public void Add(GeometricFigure figure)
+        {
+            ++_index;
+            var tmp = new GeometricFigure[_index + 1];
+            _geometricFigures.CopyTo(tmp, 0);
+            _geometricFigures = tmp;
+            _geometricFigures[_index] = figure;
+        }
+        public void Insert(int index, GeometricFigure figure)
+        {
+            if (index < 0 || index > _index)
+                throw new ArgumentOutOfRangeException();
+
+            ++_index;
+            var tmp = new GeometricFigure[_index + 1];
+            _geometricFigures.CopyTo(tmp, 0);
+            _geometricFigures = tmp;
+            for (int i = _index - 1; i >= index; i--)
+                _geometricFigures[i + 1] = _geometricFigures[i];
+
+            _geometricFigures[index] = figure;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 && index >= _index) 
+                throw new ArgumentOutOfRangeException();
+
+            _geometricFigures = _geometricFigures.Where((val, idx) => idx != index).ToArray();
+            _index--;
+        }
+        public int Search(GeometricFigure figure)
+        {
+            return Array.IndexOf(_geometricFigures, figure);
+        }
+        public void Show()
+        {
+            foreach(var item in _geometricFigures)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+        }
+
+        //Индексатор
+        public GeometricFigure this[int index]
+        {
+            get => _geometricFigures[index];
+            private set => _geometricFigures[index] = value;
+        }
+    }
+}
---
+++ Lab09/Lab09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1) Продемонстрируйте работу с коллекцией (добавление/удаление/поиск/вывод):*/
+            Console.WriteLine("--------------------------------------------------");
+            GeometricFiguresCollection collection = new GeometricFiguresCollection();
+            collection.Add(new GeometricFigure("Прямоугольник", 8));
+            collection.Add(new GeometricFigure("Тетраэдр", 20));
+            collection.Add(new GeometricFigure("Параллелепипед", 10));
+            collection.Add(new GeometricFigure("Треугольник", 6));
+            collection.Add(new GeometricFigure("Октаэдр", 11));
+            collection.Add(new GeometricFigure("Дельтоид", 9));
+            collection.Show();
+            Console.WriteLine("--------------------------------------------------");
+            collection.RemoveAt(3);
+            collection.Show();
+            Console.WriteLine("--------------------------------------------------");
+            collection.Insert(2, new GeometricFigure("Тор", 4));
+            collection.Show();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"{collection[5]}");
+
+            /*2)Создайте универсальную коллекцию в соответствии с вариантом задания и 
+            заполнить ее данными встроенного типа.Net(int, char,…).*/
+
+        }
+    }
+}
